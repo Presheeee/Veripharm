d359830e3dd874e0a126a5883d73b5a6384dff39
Precious Hayford <107083677+Presheeee@users.noreply.github.com>
2025-07-27 17:55:00 +0000

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,18 @@
+require("dotenv").config();
+
+//console.log("Loaded RPC URL:", process.env.SEPOLIA_RPC_URL);
+
 require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+    
+    networks: {
+      sepolia: {
+        url: process.env.SEPOLIA_RPC_URL, // now picks up your .env value
+        accounts: [process.env.PRIVATE_KEY], // likewise for your private key
+    },
+  }, 
+       
 };
