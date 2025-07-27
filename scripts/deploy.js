cb22e97da3ea93366dd487608dd22c597b5909ca
Precious Hayford <107083677+Presheeee@users.noreply.github.com>
2025-07-27 17:53:53 +0000

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,3 +1,22 @@
+/*
+
+Deployer account(local): 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266
+
+Deployer account(sepolia): 0x98625DEc1D20E8dD6a7966dC544bf0F59fb4390c
+
+Old infura endpoint: https://sepolia.infura.io/v3/742c30491faa4846b80928e9f2cab7e4
+
+Metamask PRIVATE_KEY: 0xc07d4bef49fa9a277bbb9413db0fe21679fc03af23c55443a93fc819f2800c9d
+
+Veripharm deplyed to: 0x5FbDB2315678afecb367f032d93F642f64180aa3
+
+*/
+
+
+console.log("RPC URL:", process.env.SEPOLIA_RPC_URL);
+
+
+
 const hre = require("hardhat");
 
 async function main() {
@@ -27,3 +46,5 @@ main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
+
+
